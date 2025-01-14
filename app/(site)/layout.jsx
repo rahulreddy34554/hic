083c2d29546039b3696ad7ec67f7b7a2a54ab189
rahulreddy34554 <rahulreddyallala6@gmail.com>
2025-01-14 12:27:36 -0500

    Add files via upload

---
+++ app/(site)/layout.jsx
@@ -0,0 +1,14 @@
+import Footer from "./_components/footer";
+import { Navbar } from "./_components/navbar";
+
+const SiteLayout = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      {children}
+      <Footer />
+    </>
+  );
+};
+
+export default SiteLayout;
